43776f60781f51b3f87f897173e3b33b8bc13da2
Sachin Kumara Liyanage <sachinkumaraliyanage@gmail.com>
2020-05-07 14:59:59 +0530

    mod: all log system

--- SAITA/IT17152938/SLIS/Util/DBConnection.py
+++ SAITA/IT17152938/SLIS/Util/DBConnection.py
@@ -1,5 +1,6 @@
 import mysql.connector
-from Data.Veriables import *
+from Data.Veriables import sql_server, sql_db, sql_uname, sql_password
+from Data.Log import *
 
 
 class DBConnection(object):
@@ -15,16 +16,19 @@ class DBConnection(object):
                 passwd=sql_password,
                 database=sql_db,
             )
+            add_log(log_types[2], "DBConnection", "Create new DB object")
 
         return cls.mydb
 
     @classmethod
     def execute_query(cls, query, value):
         """execute query on singleton db connection"""
+        add_log(log_types[2], "DBConnection", "values : "+str(value)+"  query :  "+str(query))
         connection = cls.get_connection()
         try:
             cursor = connection.cursor(dictionary=True)
         except mysql.ProgrammingError:
+            add_log(log_types[0], "DBConnection", "Get cursor so create new connection")
             connection = cls.get_connection(new=True)  # Create new connection
             cursor = connection.cursor(dictionary=True)
         cursor.execute(query, value)
