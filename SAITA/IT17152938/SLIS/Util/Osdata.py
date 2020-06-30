fa41285871ba4467a506048bb520f1289247c782
sachin kumara liyanage <it17152938@my.sliit.lk>
2020-06-30 11:41:05 +0530

    mode: comment debug line

--- SAITA/IT17152938/SLIS/Util/Osdata.py
+++ SAITA/IT17152938/SLIS/Util/Osdata.py
@@ -6,6 +6,8 @@ class Osdata:
 
     # get installed software list
     def get_installed_soft_list(self):
+        # for app in winapps_change.list_installed():
+        #     print(app.name, app.install_location, app.version)
         return winapps_change.list_installed()
 
     # search in installed list
