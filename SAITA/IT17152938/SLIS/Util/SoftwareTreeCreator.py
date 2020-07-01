bc40e4a28323f4ab27130b79cb9df95ecd2080ac
sachin kumara liyanage <it17152938@my.sliit.lk>
2020-07-01 14:10:22 +0530

    add: method to SoftwareTreeCreator.py

--- SAITA/IT17152938/SLIS/Util/SoftwareTreeCreator.py
+++ SAITA/IT17152938/SLIS/Util/SoftwareTreeCreator.py
@@ -2,8 +2,9 @@ from Util.SoftwareNode import SoftwareNode
 from Util.Osdata import Osdata
 
 
-class SoftwareTree:
+class SoftwareTreeCreator:
     tree = []
+    tree_key = []
 
     def create_tree(self, soft_list):
         osdata = Osdata()
@@ -11,25 +12,16 @@ class SoftwareTree:
         for soft in soft_list:
             ver = [soft['ver_id']]
             self.__create_node(ver, osbit)
-
-        self.__clean_duplicate()
         return self.tree
 
     def __create_node(self, ver_list, osbit):
         for ver in ver_list:
-            soft_node = SoftwareNode(ver, osbit)
-            if not soft_node.get_dependency_len() == 0:
-                self.__create_node(soft_node.get_dependency(), osbit)
-            self.tree.append(soft_node)
-
-    def __clean_duplicate(self):
-        temp_tree = {}
-        temp_id = []
-        for node in self.tree:
             try:
-                temp_id.index(node.get_ver_id())
+                self.tree_key.index(ver)
             except:
-                temp_id.append(node.get_ver_id())
-                temp_tree[node.get_ver_id()] = node
+                soft_node = SoftwareNode(ver, osbit)
+                if not soft_node.get_dependency_len() == 0:
+                    self.__create_node(soft_node.get_dependency(), osbit)
+                self.tree_key.append(ver)
+                self.tree.append(soft_node)
 
-        self.tree = temp_tree
