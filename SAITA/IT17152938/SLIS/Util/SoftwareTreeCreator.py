49786206cac73a3be469f8e05636e1adc326e1a4
sachin kumara liyanage <it17152938@my.sliit.lk>
2020-07-08 13:12:02 +0530

    add: method to tree divide using software wise

--- SAITA/IT17152938/SLIS/Util/SoftwareTreeCreator.py
+++ SAITA/IT17152938/SLIS/Util/SoftwareTreeCreator.py
@@ -12,6 +12,7 @@ class SoftwareTreeCreator:
         for soft in soft_list:
             ver = [soft['ver_id']]
             self.__create_node(ver, osbit)
+        self.__do_category()
         return self.tree
 
     def __create_node(self, ver_list, osbit):
@@ -25,3 +26,23 @@ class SoftwareTreeCreator:
                 self.tree_key.append(ver)
                 self.tree.append(soft_node)
 
+    def __do_category(self):
+        temp_tree_category = []
+        temp_tree = []
+        for main_node in self.tree:
+            try:
+                temp_tree.index(main_node)
+                continue
+            except:
+                temp_tree.append(main_node)
+            temp = [main_node]
+            for node in self.tree:
+                try:
+                    temp_tree.index(node)
+                    continue
+                except:
+                    if main_node.get_soft_id() == node.get_soft_id():
+                        temp_tree.append(node)
+                        temp.append(node)
+            temp_tree_category.append(temp)
+        self.tree = temp_tree_category
