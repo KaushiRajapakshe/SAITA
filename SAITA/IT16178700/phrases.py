54f4e4bda2fdda67a4cf854c7a7e1eae2766fb08
Kaushalya Rajapakshe <it16178700@my.sliit.lk>
2020-09-08 22:28:50 +0530

    Added update version of get regular expression

--- SAITA/IT16178700/phrases.py
+++ SAITA/IT16178700/phrases.py
@@ -1,11 +1,10 @@
-
 def get_phrases():
     keep_phrases = ["Address already in use", "Unable to open logs", "ReservedCodeCacheSize=240m",
                     "Application run failed", "objectMapperConfigurer"]
     return keep_phrases
 
 
-# time regular expression pattern
-def get_regular_expression():
+# time regular expression pattern 01 for 2020 21:02:03
+def get_regular_expression1():
     regular_expression = r"[0-9][0-9].[0-9][0-9]\:[0-9][0-9]\:[0-9][0-9]"
     return regular_expression
