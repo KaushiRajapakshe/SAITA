1f25ba2a1ae2a67c7b8718122aff52406b721896
Kaushalya Rajapakshe <it16178700@my.sliit.lk>
2020-09-08 22:32:56 +0530

    Added owl types to constants

--- SAITA/IT16178700/constant/constants.py
+++ SAITA/IT16178700/constant/constants.py
@@ -4,3 +4,17 @@ FOLDER_NAME_USERS = '/Users'
 FOLDER_NAME_LIBRARY = '/Library'
 SIMPLE_LOGS = 'logs'
 CAPITAL_LOGS = 'Logs'
+
+# OWL
+# Types
+APPLICATION = 'Application'
+CATEGORY = 'Category'
+DATABASE = 'Database'
+DIFFERENT_TYPE = 'DifferentType'
+GENRE = 'Genre'
+LOGERROR = 'LogError'
+PLATFORM = 'Platform'
+SINGLE_TYPE = 'SingleType'
+USER_ENTERED = 'UserEntered'
+WEB = 'Web'
+WINDOWS_PLATFORM = 'WindowsPlatform'
