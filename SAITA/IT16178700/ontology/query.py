b48046d82966045fcad8ebe9dd8b5ea6608180ef
Kaushalya Rajapakshe <it16178700@my.sliit.lk>
2020-09-08 22:21:28 +0530

    Added owl queries

--- SAITA/IT16178700/ontology/query.py
+++ SAITA/IT16178700/ontology/query.py
@@ -14,3 +14,15 @@ def query_by_application_name(value, application_name):
         }      
     """
     return query1
+
+
+def get_application_type():
+    query2 = """
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+        SELECT DISTINCT ?isDefinedBy
+        WHERE {
+          ?rdfs rdfs:isDefinedBy ?isDefinedBy;
+        }
+    """
+    return query2
