f14264bed388596f12ace8a5bf8a95f4f76708d5
Kaushalya Rajapakshe <it16178700@my.sliit.lk>
2020-09-09 05:06:57 +0530

    Change log extension identify implementation for scheduler

--- SAITA/IT16178700/scheduler/logidentity/log_extension_identify.py
+++ SAITA/IT16178700/scheduler/logidentity/log_extension_identify.py
@@ -1,12 +1,12 @@
 import SAITA.IT16178700.access_file_detail as access_file_detail
 import SAITA.IT16178700.scheduler.drives_list as drives_list
+import os
 
 log_extension = []
 
 
 def get_log_extension():
     for head_dir in drives_list.get_os_drives_list():
-        import os
         for root, dirs, files in os.walk(head_dir):
             for file in files:
                 if file.endswith(".log"):
@@ -14,5 +14,6 @@ def get_log_extension():
     return log_extension
 
 
-all_output_list = ['\n'.join(get_log_extension())]
-access_file_detail.add_log_details(all_output_list, "../textfile/logExtension.txt")
+def add_log_extension():
+    all_output_list = ['\n'.join(get_log_extension())]
+    access_file_detail.add_log_details(all_output_list, "../textfile/logExtension.txt")
