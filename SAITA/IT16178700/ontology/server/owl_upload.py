7d4ee2ed0fce41b1d5120aa86bf5ddc4738fecb4
Kaushalya Rajapakshe <it16178700@my.sliit.lk>
2020-09-08 22:18:57 +0530

    Added owl file upload Fuseki Server API Implementation

---
+++ SAITA/IT16178700/ontology/server/owl_upload.py
@@ -0,0 +1,15 @@
+import requests
+
+from SAITA.IT16178700.data import variables
+
+
+def uploadOWL():
+    url = variables.url + '/data'
+    data = open('../controller/saita.owl').read()
+    headers = {'Content-Type': 'application/rdf+xml; boundary=----WebKitFormBoundaryc8DYiSwZQFPx4yH1'}
+    r = requests.post(url, data=data, headers=headers)
+    if r.status_code == 200:
+        return 'Success'
+    else:
+        return 'Fail'
+
