dc6593cf5cfb0c822581cde30935e34d83784609
Kaushalya Rajapakshe <it16178700@my.sliit.lk>
2020-09-19 23:29:46 +0530

    Changed log no extension identify

--- SAITA/IT16178700/error/analysis/scheduler/logidentity/log_no_extension.py
+++ SAITA/IT16178700/error/analysis/scheduler/logidentity/log_no_extension.py
@@ -2,7 +2,7 @@ import os
 from fnmatch import fnmatch
 
 import SAITA.IT16178700.access_file_detail as access_file_detail
-import SAITA.IT16178700.scheduler.drives_list as drives_list
+import SAITA.IT16178700.error.analysis.scheduler.drives_list as drives_list
 
 pattern = "*log"
 log_no_extension = []
@@ -20,4 +20,4 @@ def get_log_no_extension():
 def add_log_no_extension_file():
     all_output_list = ['\n'.join(get_log_no_extension())]
 
-    access_file_detail.add_log_details(all_output_list, "../textfile/logNoExtension.txt")
+    access_file_detail.add_log_details(all_output_list, "../../../textfile/logNoExtension.txt")
