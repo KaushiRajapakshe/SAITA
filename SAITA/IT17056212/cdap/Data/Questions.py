73c4caef356e84daa6f3dcc534cf6c0bafd6dafa
P.M.C.P.Paththinisekara <pmchamikaprabath@gmail.com>
2020-10-09 15:29:23 +0530

    impl: rank log

--- SAITA/IT17056212/cdap/Data/Questions.py
+++ SAITA/IT17056212/cdap/Data/Questions.py
@@ -6,8 +6,8 @@ directory_categorizing_questions = ["Did system showed any issue indications?",
                                     "Is the related drive listed in the computer?",
                                     "Did you previously worked with this functionality?",
                                     "Did you notice any anomalies in the computer?", "result"]
-userconf_categorizing_questions = ["Do you have access permission for this function>",
-                                   "Is the related account listed in the computer",
+userconf_categorizing_questions = ["Do you have access permission for this function?",
+                                   "Is the related account listed in the computer?",
                                    "Did you previously worked with this functionality?",
                                    "Did you notice any anomalies in the computer?", "result"]
 
@@ -22,3 +22,4 @@ directory_identifying_questions = ["What is the Error message displayed on the s
                                    "What is the Error code or number displayed on the screen?",
                                    "What is your error related type (File/Directory/Drive)?",
                                    "Please provide File/Directory/Drive path", "result"]
+Ranking_question="Please rate the previous solution on a rating scale of 1-5, where 1 is ‘Not Helpful’ and 5 is ‘Very Helpful':"
