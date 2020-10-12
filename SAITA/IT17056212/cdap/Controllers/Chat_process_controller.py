949e4369f03e03078e10a78813341903bfd58d13
P.M.C.P.Paththinisekara <pmchamikaprabath@gmail.com>
2020-10-12 12:55:11 +0530

    changes: Controller improvements

--- SAITA/IT17056212/cdap/Controllers/Chat_process_controller.py
+++ SAITA/IT17056212/cdap/Controllers/Chat_process_controller.py
@@ -1,8 +1,12 @@
+import csv
+
 from Controllers.Decisiontree_controller import DecisiontreeController
 from Controllers.Query import Queries
 from Controllers.Sentence_Controller import SentenceController
 from Data.Variables import *
 from Models.Inputs import Input
+from Controllers.Ranking_controller import RankingController
+from Util.ScriptGenerator import ScriptGenerator
 
 
 class mainController:
@@ -16,7 +20,7 @@ class mainController:
     def set_input(self, userreply):
         self.input = userreply
 
-    def process(self):
+    def process(self, acc_ra, work_area, win_root):
         print(self.input)
 
         for cat_in in network_synonyms:
@@ -28,6 +32,11 @@ class mainController:
                     if cat_in2 in self.input[0].lower():
                         category = 'directory'
                         break
+                    else:
+                        for cat_in3 in userconf_synonyms:
+                            if cat_in3 in self.input[0].lower():
+                                category = 'user'
+                                break
 
         self.inp.set_category(category)
 
@@ -40,9 +49,60 @@ class mainController:
                 self.inp.set_error_code(self.input[2])
                 self.inp.set_type(self.input[3])
 
+            elif self.inp.get_category() == 'directory':
+                self.inp.set_error_msg(self.input[1])
+                self.inp.set_error_code(self.input[2])
+                self.inp.set_type(self.input[3])
+                self.inp.set_path_para(self.input[4])
+
+            elif self.inp.get_category() == 'user':
+                self.inp.set_error_msg(self.input[1])
+                self.inp.set_error_code(self.input[2])
+                self.inp.set_type(self.input[3])
+                self.inp.set_name_para(self.input[4])
+
         predicted_issue_ids = DecisiontreeController.decisontree_results(self.inp.get_component(),
                                                                          self.inp.get_category(),
                                                                          self.inp.get_input_array())
+
+        # send issue list for ranking
+        rank = RankingController()
+        issue_id = rank.process_decisiontree_result(predicted_issue_ids)
+        print(issue_id)
+
+        # create tempory ranking info on rank_log.csv
+        cat = self.inp.get_category()
+        typ = self.inp.get_type()
+
+        itype_rank = None
+        category_rank = None
+        if cat == 'network':
+            category_rank = 1
+            if typ == 'intranet':
+                itype_rank = 1
+            elif typ == 'internet':
+                itype_rank = 2
+        elif cat == 'directory':
+            category_rank = 2
+            if typ == 'file':
+                itype_rank = 3
+            elif typ == 'directory':
+                itype_rank = 4
+            elif typ == 'drive':
+                itype_rank = 5
+        elif cat == 'user':
+            category_rank = 3
+            if typ == 'local':
+                itype_rank = 6
+
+        w, x, y = category_rank, itype_rank, issue_id
+        csvRow = [w, x, y]
+        csvfile = rank_log_csv
+        with open(csvfile, "a", newline='') as fp1:
+            wr = csv.writer(fp1, dialect='excel')
+            wr.writerow(csvRow)
+        fp1.close()
+
         component = 'categorizing'
         self.inp.set_component(component)
 
@@ -53,25 +113,42 @@ class mainController:
                 self.inp.set_question3(self.input[6])
                 self.inp.set_question4(self.input[7])
 
+            elif self.inp.get_category() == 'directory':
+                self.inp.set_question1(self.input[5])
+                self.inp.set_question2(self.input[6])
+                self.inp.set_question3(self.input[7])
+                self.inp.set_question4(self.input[8])
+
+            elif self.inp.get_category() == 'user':
+                self.inp.set_question1(self.input[5])
+                self.inp.set_question2(self.input[6])
+                self.inp.set_question3(self.input[7])
+                self.inp.set_question4(self.input[8])
+
         predicted_solution_type = DecisiontreeController.decisontree_results(self.inp.get_component(),
                                                                              self.inp.get_category(),
                                                                              self.inp.get_input_array())
         solution_id = None
-        for key in predicted_issue_ids.keys():
-            print(key)
-            issue_id = key
-            for key2 in predicted_solution_type.keys():
-                print(key2)
-                solution_type = key2
-                if solution_type == "non-tech":
-                    solution_id = Queries.get_nontech_solution_id_by_issue_id(issue_id)
+        for key2 in predicted_solution_type.keys():
+            print(key2)
+            solution_type = key2
+            if solution_type == "non-tech":
+                solution_id = Queries.get_nontech_solution_id_by_issue_id(issue_id)
 
+                print(solution_id[0][0])
+                nontech_keyward = Queries.get_keywards_by_nontech_solution_id(solution_id[0][0])
+                print(nontech_keyward)
+                sentence = SentenceController.sentence_generator_results(nontech_keyward)
+                print("Main Output: " + sentence)
+                return sentence
+            elif solution_type == "tech":
+                solution_id = Queries.get_tech_solution_id_by_issue_id(issue_id)
+                permant_para = Queries.get_permenant_parameter_by_issue_id(issue_id)
+                if permant_para[0][0] != 'None':
+                    print(permant_para[0][0])
+                    self.inp.set_name_para(permant_para[0][0])
+                else:
                     print(solution_id[0][0])
-                    nontech_keyward = Queries.get_keywards_by_nontech_solution_id(solution_id[0][0])
-                    sentence = SentenceController.sentence_generator_results(nontech_keyward)
-                    print("Main Output: " + sentence)
+                y = ScriptGenerator()
 
-                elif solution_type == "tech":
-                    solution_id = Queries.get_tech_solution_id_by_issue_id(issue_id)
-                    print(solution_id[0][0])
-        return sentence
+                return y.process_sequence(solution_id[0][0], acc_ra, work_area, win_root)
--- SAITA/IT17056212/cdap/Controllers/Main.py
+++ SAITA/IT17056212/cdap/Controllers/Main.py
@@ -1,3 +1,5 @@
+import csv
+
 from Controllers.Ranking_controller import RankingController
 from Data import Questions
 from Models.Inputs import Input
@@ -24,147 +26,225 @@ if __name__ == '__main__':
     userconf_ident_ques.pop(len(userconf_ident_ques) - 1)
     userconf_cat_ques.pop(len(userconf_cat_ques) - 1)
 
-    # for x in range(6):
-    category_input = input("What type of issue do you have(network/file and directory/User configuration)? ")
-    for cat_in in network_synonyms:
-        if cat_in in category_input.lower():
-            category = 'network'
-            break
-        else:
-            for cat_in2 in directory_synonyms:
-                if cat_in2 in category_input.lower():
-                    category = 'directory'
+    with open(rank_log_csv) as f2:
+        check_emp = f2.readline()
+        if check_emp == '':
+            print('Empty rank_log')
+
+            # for x in range(6):
+            category_input = input("What type of issue do you have(network/file and directory/User configuration)? ")
+            for cat_in in network_synonyms:
+                if cat_in in category_input.lower():
+                    category = 'network'
                     break
                 else:
-                    for cat_in3 in userconf_synonyms:
-                        if cat_in3 in category_input.lower():
-                            category = 'user'
+                    for cat_in2 in directory_synonyms:
+                        if cat_in2 in category_input.lower():
+                            category = 'directory'
                             break
+                        else:
+                            for cat_in3 in userconf_synonyms:
+                                if cat_in3 in category_input.lower():
+                                    category = 'user'
+                                    break
 
-    inp.set_category(category)
+            inp.set_category(category)
 
-    component = 'identifying'
-    inp.set_component(component)
+            component = 'identifying'
+            inp.set_component(component)
 
-    if inp.get_component() == 'identifying':
-        if inp.get_category() == 'network':
-            msg = input(net_ident_ques[0])
-            inp.set_error_msg(msg)
+            if inp.get_component() == 'identifying':
+                if inp.get_category() == 'network':
+                    msg = input(net_ident_ques[0])
+                    inp.set_error_msg(msg)
 
-            code = input(net_ident_ques[1])
-            inp.set_error_code(code)
+                    code = input(net_ident_ques[1])
+                    inp.set_error_code(code)
 
-            type = input(net_ident_ques[2])
-            inp.set_type(type)
+                    type = input(net_ident_ques[2])
+                    inp.set_type(type)
 
-        elif inp.get_category() == 'directory':
-            msg = input(direct_ident_ques[0])
-            inp.set_error_msg(msg)
+                elif inp.get_category() == 'directory':
+                    msg = input(direct_ident_ques[0])
+                    inp.set_error_msg(msg)
 
-            code = input(direct_ident_ques[1])
-            inp.set_error_code(code)
+                    code = input(direct_ident_ques[1])
+                    inp.set_error_code(code)
 
-            type = input(direct_ident_ques[2])
-            inp.set_type(type)
+                    type = input(direct_ident_ques[2])
+                    inp.set_type(type)
 
-            path_par = input(direct_ident_ques[3])
-            inp.set_path_para(path_par)
+                    path_par = input(direct_ident_ques[3])
+                    inp.set_path_para(path_par)
 
-        elif inp.get_category() == 'user':
-            msg = input(userconf_ident_ques[0])
-            inp.set_error_msg(msg)
+                elif inp.get_category() == 'user':
+                    msg = input(userconf_ident_ques[0])
+                    inp.set_error_msg(msg)
 
-            code = input(userconf_ident_ques[1])
-            inp.set_error_code(code)
+                    code = input(userconf_ident_ques[1])
+                    inp.set_error_code(code)
 
-            type = input(userconf_ident_ques[2])
-            inp.set_type(type)
+                    type = input(userconf_ident_ques[2])
+                    inp.set_type(type)
 
-            usr_name = input(userconf_ident_ques[3])
-            inp.set_name_para(usr_name)
+                    usr_name = input(userconf_ident_ques[3])
+                    inp.set_name_para(usr_name)
 
-    predicted_issue_ids = DecisiontreeController.decisontree_results(inp.get_component(), inp.get_category(),
-                                                                     inp.get_input_array())
-    rank = RankingController()
-    issue_id = rank.process_decisiontree_result(predicted_issue_ids)
-    print(issue_id)
+            predicted_issue_ids = DecisiontreeController.decisontree_results(inp.get_component(), inp.get_category(),
+                                                                             inp.get_input_array())
+            rank = RankingController()
+            issue_id = rank.process_decisiontree_result(predicted_issue_ids)
+            print(issue_id)
 
-    component = 'categorizing'
-    inp.set_component(component)
+            cat = inp.get_category()
+            typ = inp.get_type()
 
-    if inp.get_component() == 'categorizing':
-        if inp.get_category() == 'network':
-            q1 = input(net_cat_ques[0])
-            inp.set_question1(q1)
+            itype_rank = None
+            category_rank = None
+            if cat == 'network':
+                category_rank = 1
+                if typ == 'intranet':
+                    itype_rank = 1
+                elif typ == 'internet':
+                    itype_rank = 2
+            elif cat == 'directory':
+                category_rank = 2
+                if typ == 'file':
+                    itype_rank = 3
+                elif typ == 'directory':
+                    itype_rank = 4
+                elif typ == 'drive':
+                    itype_rank = 5
+            elif cat == 'user':
+                category_rank = 3
+                if typ == 'local':
+                    itype_rank = 6
 
-            q2 = input(net_cat_ques[1])
-            inp.set_question2(q2)
+            w, x, y = category_rank, itype_rank, issue_id
+            csvRow = [w, x, y]
+            csvfile = rank_log_csv
+            with open(csvfile, "a", newline='') as fp1:
+                wr = csv.writer(fp1, dialect='excel')
+                wr.writerow(csvRow)
+            fp1.close()
 
-            q3 = input(net_cat_ques[2])
-            inp.set_question3(q3)
+            component = 'categorizing'
+            inp.set_component(component)
 
-            q4 = input(net_cat_ques[3])
-            inp.set_question4(q4)
+            if inp.get_component() == 'categorizing':
+                if inp.get_category() == 'network':
+                    q1 = input(net_cat_ques[0])
+                    inp.set_question1(q1)
 
-        elif inp.get_category() == 'directory':
-            q1 = input(direct_cat_ques[0])
-            inp.set_question1(q1)
+                    q2 = input(net_cat_ques[1])
+                    inp.set_question2(q2)
 
-            q2 = input(direct_cat_ques[1])
-            inp.set_question2(q2)
+                    q3 = input(net_cat_ques[2])
+                    inp.set_question3(q3)
 
-            q3 = input(direct_cat_ques[2])
-            inp.set_question3(q3)
+                    q4 = input(net_cat_ques[3])
+                    inp.set_question4(q4)
 
-            q4 = input(direct_cat_ques[3])
-            inp.set_question4(q4)
+                elif inp.get_category() == 'directory':
+                    q1 = input(direct_cat_ques[0])
+                    inp.set_question1(q1)
 
-        elif inp.get_category() == 'user':
-            q1 = input(userconf_cat_ques[0])
-            inp.set_question1(q1)
+                    q2 = input(direct_cat_ques[1])
+                    inp.set_question2(q2)
 
-            q2 = input(userconf_cat_ques[1])
-            inp.set_question2(q2)
+                    q3 = input(direct_cat_ques[2])
+                    inp.set_question3(q3)
 
-            q3 = input(userconf_cat_ques[2])
-            inp.set_question3(q3)
+                    q4 = input(direct_cat_ques[3])
+                    inp.set_question4(q4)
 
-            q4 = input(userconf_cat_ques[3])
-            inp.set_question4(q4)
+                elif inp.get_category() == 'user':
+                    q1 = input(userconf_cat_ques[0])
+                    inp.set_question1(q1)
 
-    predicted_solution_type = DecisiontreeController.decisontree_results(inp.get_component(), inp.get_category(),
-                                                                         inp.get_input_array())
-    solution_id = None
-    #for key in predicted_issue_ids.keys():
-      #  print(key)
-       # issue_id = key
-    for key2 in predicted_solution_type.keys():
-        print(key2)
-        solution_type = key2
-        if solution_type == "non-tech":
-            solution_id = Queries.get_nontech_solution_id_by_issue_id(issue_id)
+                    q2 = input(userconf_cat_ques[1])
+                    inp.set_question2(q2)
 
-            print(solution_id[0][0])
-            nontech_keyward = Queries.get_keywards_by_nontech_solution_id(solution_id[0][0])
-            print(nontech_keyward)
-            sentence = SentenceController.sentence_generator_results(nontech_keyward)
-            print("Main Output: " + sentence)
+                    q3 = input(userconf_cat_ques[2])
+                    inp.set_question3(q3)
 
-        elif solution_type == "tech":
-            solution_id = Queries.get_tech_solution_id_by_issue_id(issue_id)
-            permant_para = Queries.get_permenant_parameter_by_issue_id(issue_id)
-            if permant_para[0][0] != 'None':
-                print(permant_para[0][0])
-                inp.set_name_para(permant_para[0][0])
-            else:
-                print(solution_id[0][0])
-            # y = ScriptGenerator()
-            # y.process_sequence(solution_id[0][0])
+                    q4 = input(userconf_cat_ques[3])
+                    inp.set_question4(q4)
 
-    # print(predicted_issue_ids)
-    # split_by_issue = predicted_issue_ids.split(",")
+            predicted_solution_type = DecisiontreeController.decisontree_results(inp.get_component(),
+                                                                                 inp.get_category(),
+                                                                                 inp.get_input_array())
+            solution_id = None
+            # for key in predicted_issue_ids.keys():
+            #  print(key)
+            # issue_id = key
+            for key2 in predicted_solution_type.keys():
+                print(key2)
+                solution_type = key2
+                if solution_type == "non-tech":
+                    solution_id = Queries.get_nontech_solution_id_by_issue_id(issue_id)
 
-    # for y in split_by_issue:
-    #   split_by_issue_id = y.split(":")
-    #   issue_id = split_by_issue_id[0]
-    #  print(issue_id)
+                    print(solution_id[0][0])
+                    nontech_keyward = Queries.get_keywards_by_nontech_solution_id(solution_id[0][0])
+                    print(nontech_keyward)
+                    sentence = SentenceController.sentence_generator_results(nontech_keyward)
+                    print("Main Output: " + sentence)
+
+                elif solution_type == "tech":
+                    solution_id = Queries.get_tech_solution_id_by_issue_id(issue_id)
+                    permant_para = Queries.get_permenant_parameter_by_issue_id(issue_id)
+                    if permant_para[0][0] != 'None':
+                        print(permant_para[0][0])
+                        inp.set_name_para(permant_para[0][0])
+                    else:
+                        print(solution_id[0][0])
+                    # y = ScriptGenerator()
+                    # y.process_sequence(solution_id[0][0])
+
+                ranking_q = input(Questions.Ranking_question)  # ranking question after solution
+                if 5 >= int(ranking_q) >= 1:
+                    with open(rank_log_csv, newline='') as f1:
+                        reader = csv.reader(f1)
+                        row1 = next(reader)
+                    f1.close()
+                    print(row1)
+
+                    w, x, y, z = row1[0], row1[1], row1[2], int(ranking_q)
+                    csvRow = [w, x, y, z]
+                    csvfile = issue_history_csv
+                    with open(csvfile, "a", newline='') as fp:
+                        wr = csv.writer(fp, dialect='excel')
+                        wr.writerow(csvRow)
+                    fp.close()
+
+                    f3 = open(rank_log_csv, "w")
+                    f3.truncate()
+                    f3.close()
+
+            # print(predicted_issue_ids)
+            # split_by_issue = predicted_issue_ids.split(",")
+
+            # for y in split_by_issue:
+            #   split_by_issue_id = y.split(":")
+            #   issue_id = split_by_issue_id[0]
+            #  print(issue_id)
+        else:
+            ranking_q = input(Questions.Ranking_question)  # ranking question after reboot
+            if 5 >= int(ranking_q) >= 1:
+                with open(rank_log_csv, newline='') as f1:
+                    reader = csv.reader(f1)
+                    row1 = next(reader)
+                f1.close()
+                print(row1)
+
+                w, x, y, z = row1[0], row1[1], row1[2], int(ranking_q)
+                csvRow = [w, x, y, z]
+                csvfile = issue_history_csv
+                with open(csvfile, "a", newline='') as fp:
+                    wr = csv.writer(fp, dialect='excel')
+                    wr.writerow(csvRow)
+                fp.close()
+
+                f3 = open(rank_log_csv, "w")
+                f3.truncate()
+                f3.close()
