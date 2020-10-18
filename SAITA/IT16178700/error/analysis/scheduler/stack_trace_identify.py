deb0066c2737ccc55edc55ca034d51c426b4731a
Kaushalya Rajapakshe <it16178700@my.sliit.lk>
2020-10-18 16:35:22 +0530

    Added updated stack trace identify implementation

--- SAITA/IT16178700/error/analysis/scheduler/stack_trace_identify.py
+++ SAITA/IT16178700/error/analysis/scheduler/stack_trace_identify.py
@@ -4,12 +4,13 @@ import re
 import SAITA.IT16178700.access_file_detail as access_file_detail
 import SAITA.IT16178700.phrases as phrases
 from SAITA.IT16178700.abstracterror import error_detail
+from SAITA.IT16178700.model.Error import Error
 
 
 def get_log_file():
-    log_files = [access_file_detail.view_log_details("../../../textfile/logExtension.txt"),
-                 access_file_detail.view_log_details("../../../textfile/logMatch.txt"),
-                 access_file_detail.view_log_details("../../../textfile/logNoExtension.txt")]  # set logs txt file list
+    log_files = [access_file_detail.view_log_details("../textfile/logExtension.txt"),
+                 access_file_detail.view_log_details("../textfile/logMatch.txt"),
+                 access_file_detail.view_log_details("../textfile/logNoExtension.txt")]  # set logs txt file list
     return log_files
 
 
@@ -26,7 +27,9 @@ def identify_stack_trace():
     expression2 = re.compile(regular_expression2)
     count = 0
     length = 0
-
+    error_list = []
+    set_phrase = ''
+    set_log = ''
     # identify stack trace on error
     for logs in get_log_file():
         for infile in logs:
@@ -34,19 +37,26 @@ def identify_stack_trace():
                 last_char = infile[-1]
                 if last_char == "\n":
                     infile = infile[:-1]
-
+                last_ch = log[-1:]
+                if last_ch == "\n":
+                    log = log[:-1]
                 # See if the path exists.
                 if os.path.exists(log):
                     # open file
                     try:
                         with open(log, 'r') as f:
                             f = f.readlines()
+                            error = Error()
                             for line in f:
                                 length += 1
                                 if expression1.search(line) or length == len(f) or expression2.search(line):
                                     if count == 1:
                                         count = 0
                                         important.append(stack_trace)
+                                        error.set_log_stack_trace(stack_trace)
+                                        error.set_error_description(set_phrase)
+                                        error.set_log_path(set_log)
+                                        error_list = check_error_list(error, error_list)
                                         error_detail.error_details(stack_trace, log)
                                         stack_trace = []
                                     stack_trace = [line]
@@ -54,8 +64,11 @@ def identify_stack_trace():
                                     stack_trace.append(line)
                                 for phrase in keep_phrases:
                                     if phrase in line:
+                                        set_phrase = phrase
+                                        set_log = log
                                         count = 1
                         length = 0
+                        # error_list.append(error)
                     except (IOError, PermissionError, UnicodeDecodeError) as e:
                         # print("Error : ", e) // want to write log
                         pass
@@ -65,3 +78,20 @@ def identify_stack_trace():
             print(line)
         print(
             '##########################################################################################################')
+    return error_list
+
+
+def check_error_list(error, error_list):
+    count = 0
+    log_true = 1
+    if not error_list:
+        error_list.append(error)
+    else:
+        for e in error_list:
+            count += 1
+            if e.get_log_path() in error.get_log_path() and e.get_error_description() in error.get_error_description():
+                log_true = 2
+                pass
+            elif log_true != 2 and len(error_list) == count:
+                error_list.append(error)
+    return error_list
